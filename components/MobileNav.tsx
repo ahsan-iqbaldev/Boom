584daf6b3d7beed6c1acf3e09f9bd37cfaed6b1f
Ahsan Iqbal <150703987+codesbyahsan@users.noreply.github.com>
2024-04-12 06:08:10 +0500

    create mobile navbar

---
+++ components/MobileNav.tsx
@@ -0,0 +1,79 @@
+"use client";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "./constants";
+import { cn } from "@/lib/utils";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={36}
+            height={36}
+            alt="hamburger Icon"
+            className="max-sm:size-10"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-dark-1">
+          <Link href="/" className="flex items-center gap-1">
+            <Image
+              src="/icons/logo.svg"
+              alt="Boom Logo"
+              width={32}
+              height={32}
+            />
+            <p className="text-[26px] font-extrabold text-white">Boom</p>
+          </Link>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <section className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((link) => {
+                  const isActive =
+                    pathname === link.route;
+                  return (
+                    <SheetClose asChild key={link.route}>
+                      <Link
+                        href={link.route}
+                        key={link.label}
+                        className={cn(
+                          "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
+                          {
+                            "bg-blue-1": isActive,
+                          }
+                        )}
+                      >
+                        <Image
+                          src={link.imageUrl}
+                          alt={link.label}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">{link.label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
