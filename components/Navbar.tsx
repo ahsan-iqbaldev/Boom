c10bd5c3c9f558890dc8cbd5a7dee10350778ec1
Ahsan Iqbal <150703987+codesbyahsan@users.noreply.github.com>
2024-04-12 06:07:37 +0500

    create navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src="/icons/logo.svg" alt="Boom Logo" width={32} height={32} />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Boom
+        </p>
+      </Link>
 
-export default Navbar
+      <div className="flex-between gap-5">
+        <UserButton />
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
