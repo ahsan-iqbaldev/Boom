912ed4e348a600a464ef5c608debb713115c136b
Ahsan Iqbal <150703987+codesbyahsan@users.noreply.github.com>
2024-04-12 06:14:32 +0500

    create Reusable modal

---
+++ components/MeetingModal.tsx
@@ -0,0 +1,64 @@
+import React, { ReactNode } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+
+interface MeetingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  className?: string;
+  children?: ReactNode;
+  image?: string;
+  buttonIcon?: string;
+  buttonText?: string;
+  handleClick?: () => void;
+}
+
+const MeetingModal = ({
+  isOpen,
+  onClose,
+  title,
+  className,
+  children,
+  image,
+  buttonIcon,
+  buttonText,
+  handleClick,
+}: MeetingModalProps) => {
+  return (
+    <div>
+      <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+          <div className="flex flex-col gap-6">
+            {image && (
+              <div className="flex justify-center">
+                <Image src={image} alt="Image" width={72} height={72} />
+              </div>
+            )}
+            <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+              {title}
+            </h1>
+            {children}
+            <Button className="bg-blue-1 focus-visible: ring-0 focus-visible:ring-offset-0" onClick={handleClick}>
+                {buttonIcon && (
+                    <Image src={buttonIcon} alt="button Icon" width={13} height={13}/>
+                )} &nbsp;
+                {buttonText || 'Schedule Meeting'}
+                </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default MeetingModal;
